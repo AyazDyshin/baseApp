e6ea866b3c45bf9bce11eea5027daeeec78a3352
AyazDyshin <ayazdyshin@gmail.com>
2021-09-29 00:29:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import {
   getSolidDataset,
   overwriteFile,
   getFile,
-} from "./@inrupt/solid-client";
-import { Session } from "./node_modules/@inrupt/solid-client-authn-browser";
+} from "@inrupt/solid-client";
+import { Session } from "@inrupt/solid-client-authn-browser";
 // import { deleteFile } from "./baseApp/node_modules/@inrupt/solid-client";
 // import { deleteFile } from "./baseApp/node_modules/@inrupt/solid-client/dist/index";
-import { deleteFile } from "./baseApp/node_modules/@inrupt/solid-client/dist/index.js";
+import { deleteFile } from "@inrupt/solid-client";
 // import { deleteFile } from "./node_modules/@inrupt/solid-client/dist/index.js";
 // import { deleteFile } from "./node_modules/@inrupt/solid-client/dist/index";
 
